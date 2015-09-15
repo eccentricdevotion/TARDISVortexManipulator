3e51d6e2636ef2fcb0fed793a9ef858602ba65da
eccentricdevotion <eccentricdevotion@gmail.com>
2015-09-15 21:01:54 +1200

    Add SQLite -> MySQL database converter

--- src/main/java/me/eccentric_nz/tardisvortexmanipulator/database/TVMMySQL.java
+++ src/main/java/me/eccentric_nz/tardisvortexmanipulator/database/TVMMySQL.java
@@ -32,21 +32,10 @@ public class TVMMySQL {
             service.testConnection(connection);
             statement = connection.createStatement();
 
-            // Table structure for table 'saves'
-            String querySaves = "CREATE TABLE IF NOT EXISTS saves (save_id int(11) NOT NULL AUTO_INCREMENT, uuid varchar(48) DEFAULT '', save_name varchar(64) DEFAULT '', world varchar(64) DEFAULT '', x float DEFAULT '0', y float DEFAULT '0', z float DEFAULT '0', yaw float DEFAULT '0', pitch float DEFAULT '0', PRIMARY KEY (save_id)) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;";
-            statement.executeUpdate(querySaves);
-
-            // Table structure for table 'saves'
-            String queryMessages = "CREATE TABLE IF NOT EXISTS messages (message_id int(11) NOT NULL AUTO_INCREMENT, uuid_to varchar(48) DEFAULT '', uuid_from varchar(48) DEFAULT '', message text NULL, date bigint(20), `read` int(1) DEFAULT '0', PRIMARY KEY (message_id)) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;";
-            statement.executeUpdate(queryMessages);
-
-            // Table structure for table 'beacon'
-            String queryBeacons = "CREATE TABLE IF NOT EXISTS beacons (beacon_id int(11) NOT NULL AUTO_INCREMENT, uuid varchar(48) DEFAULT '', location varchar(512) DEFAULT '', block_type varchar(32) DEFAULT '', data int(2) DEFAULT '0', PRIMARY KEY (beacon_id)) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;";
-            statement.executeUpdate(queryBeacons);
-
-            // Table structure for table 'manipulator'
-            String queryManipulator = "CREATE TABLE IF NOT EXISTS manipulator (uuid varchar(48) NOT NULL, tachyon_level int(11) DEFAULT '0', PRIMARY KEY (uuid)) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;";
-            statement.executeUpdate(queryManipulator);
+            for (String query : SQL.CREATES) {
+                String subbed = String.format(query, plugin.getConfig().getString("storage.mysql.prefix"));
+                statement.executeUpdate(subbed);
+            }
 
         } catch (SQLException e) {
             plugin.getServer().getConsoleSender().sendMessage(plugin.getPluginName() + "MySQL create table error: " + e);
